4fa3e9368f696329dc16fef02badcc06bf42c0ac
Gabriel Armas <armasgabriel29@gmail.com>
2024-03-02 19:47:55 -0600

    Se agrego el combobox de la vista de nueva colaboracion, registrar colaboracion

--- Prototipos-Coil-VIC/src/main/java/com/example/prototiposcoilvic/FXMLVentanaNuevaColabProfesorController.java
+++ Prototipos-Coil-VIC/src/main/java/com/example/prototiposcoilvic/FXMLVentanaNuevaColabProfesorController.java
@@ -1,22 +1,23 @@
 package com.example.prototiposcoilvic;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class FXMLVentanaNuevaColabProfesorController {
+public class FXMLVentanaNuevaColabProfesorController implements Initializable {
 
     @FXML
     public TextArea textAreaObjetivoGeneral;
@@ -34,6 +35,13 @@ public class FXMLVentanaNuevaColabProfesorController {
     public TextField textFieldNombreColaboracion;
     @FXML
     private Pane PanelExpandible;
+    @FXML
+    private ComboBox<String> comboBoxRegistrarColaboracion;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        comboBoxRegistrarColaboracion.setItems(FXCollections.observableArrayList("Registrar desde cero", "Registrar a partir de la oferta"));
+    }
 
     @FXML
     public void aumentarTamaño() {
@@ -106,7 +114,6 @@ public class FXMLVentanaNuevaColabProfesorController {
             alert.showAndWait();
         }
     }
-
     @FXML
     public void botonCancelar(ActionEvent event) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -117,7 +124,6 @@ public class FXMLVentanaNuevaColabProfesorController {
     }
 
     private boolean camposVacios() {
-
         TextField[] camposField = {textFieldIdioma, textFieldExperienciaEducativa, textFieldNombreColaboracion, textFieldModalidad, textFieldNombreColaboracion, textFieldTipoColaboracion};
         TextArea[] camposArea = {textAreaObjetivoGeneral, textAreaTemaInteres};
 
