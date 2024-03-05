e6c37b17f16a5b51f8fbd5b8e3b151da79c5f03c
Mauricinio7 <miory.salas@hotmail.com>
2024-03-05 09:13:44 -0600

    Errores corregidos

--- Prototipos-Coil-VIC/src/main/java/com/example/prototiposcoilvic/FXMLSistemaCOILApplication.java
+++ Prototipos-Coil-VIC/src/main/java/com/example/prototiposcoilvic/FXMLSistemaCOILApplication.java
@@ -15,18 +15,18 @@ public class FXMLSistemaCOILApplication extends Application {
 //        stage.setScene(scene);
 //        stage.show();
 
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FXMLVentanaPrincipalProfesor.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-//        stage.setTitle("Sistema COIL");
-//        stage.setScene(scene);
-//        stage.show();
-
-
-        FXMLLoader fxmlLoaderCoordinador = new FXMLLoader(HelloApplication.class.getResource("FXMLVentanaCoordinador.fxml"));
-        Scene scene2 = new Scene(fxmlLoaderCoordinador.load(), 700, 500);
-        stage.setTitle("Sistema COIL Coordinador");
-        stage.setScene(scene2);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FXMLVentanaPrincipalProfesor.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+        stage.setTitle("Sistema COIL");
+        stage.setScene(scene);
         stage.show();
+
+
+//        FXMLLoader fxmlLoaderCoordinador = new FXMLLoader(HelloApplication.class.getResource("FXMLVentanaCoordinador.fxml"));
+//        Scene scene2 = new Scene(fxmlLoaderCoordinador.load(), 700, 500);
+//        stage.setTitle("Sistema COIL Coordinador");
+//        stage.setScene(scene2);
+//        stage.show();
 
 //        FXMLLoader fxmlLoaderCoordinador = new FXMLLoader(HelloApplication.class.getResource("FXMLVentanaHistorial.fxml"));
 //        Scene scene2 = new Scene(fxmlLoaderCoordinador.load(), 700, 500);
